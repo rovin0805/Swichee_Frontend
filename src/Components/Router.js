70ee0cc207c822383d8bc9a145733a17e13fe64b
rovin0805 <qhdrn67@naver.com>
2020-12-31 17:58:20 +0900

    added Detail Route in Router

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -4,6 +4,7 @@ import Home from "Routes/Home";
 import Search from "Routes/Search";
 import Company from "Routes/Company";
 import Header from "Components/Header";
+import Detail from "Routes/Detail";
 
 const Router = () => (
   <BrowserRouter>
@@ -13,6 +14,7 @@ const Router = () => (
         <Route path="/" exact component={Home} />
         <Route path="/search" component={Search} />
         <Route path="/company" component={Company} />
+        <Route path="/posting/:id" component={Detail} />
         <Redirect from="*" to="/" />
       </Switch>
     </>
