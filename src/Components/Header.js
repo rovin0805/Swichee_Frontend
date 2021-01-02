6cf4cf92bbf1fd270b08c33ffb7991960e974517
rovin0805 <qhdrn67@naver.com>
2021-01-02 18:26:13 +0900

    Global styles, 스위치 사진  추가 및 Header css 작업

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,51 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
+import styled from "styled-components";
+
+// CSS
+const CssHeader = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+`;
+
+const List = styled.ul`
+  display: flex;
+`;
+
+const Item = styled.li`
+  width: 80px;
+  height: 50px;
+  text-align: center;
+  border-bottom: 3px solid
+    ${(props) => (props.current ? "#FF9900" : "transparent")};
+`;
+
+const CssLink = styled(Link)`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const Header = ({ location: { pathname } }) => (
-  <header>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/search">Search</Link>
-      </li>
-      <li>
-        <Link to="/company">Company Introduction</Link>
-      </li>
-    </ul>
-  </header>
+  <CssHeader>
+    <List>
+      <Item current={pathname === "/"}>
+        <CssLink to="/">Home</CssLink>
+      </Item>
+      <Item current={pathname === "/search"}>
+        <CssLink to="/search">Search</CssLink>
+      </Item>
+      <Item current={pathname === "/company"}>
+        <CssLink to="/company">Company</CssLink>
+      </Item>
+    </List>
+  </CssHeader>
 );
 
 export default withRouter(Header);
