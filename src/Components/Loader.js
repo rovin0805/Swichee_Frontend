61296e363de72563be7c1f78e653b5f3cf46ce02
rovin0805 <qhdrn67@naver.com>
2021-01-08 04:30:21 +0900

    loader css 적용

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -3,22 +3,90 @@ import styled from "styled-components";
 import { VscLoading } from "react-icons/vsc";
 
 const Container = styled.div`
-  height: 90vh;
+  /* height: 90vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 28px;
   margin-top: 20px;
-  color: #525252;
+  color: #525252; */
+  height: 90vh;
+  width: 100vw;
+  font-family: "Cabin Condensed", sans-serif;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SVG = styled.svg`
+  font-weight: bold;
+  max-width: 600px;
+  height: auto;
 `;
 
 const Loader = () => (
+  // <Container>
+  //   <span role="img" aria-label="Loading">
+  //     Loading...
+  //     <VscLoading style={{ marginLeft: 5 }} />
+  //   </span>
+  // </Container>
   <Container>
-    <span role="img" aria-label="Loading">
-      Loading...
-      <VscLoading style={{ marginLeft: 5 }} />
-    </span>
+    <SVG viewBox="0 0 100 20">
+      <defs>
+        <linearGradient id="gradient" x1="0" x2="0" y1="0" y2="1">
+          <stop offset="10%" stopColor="#FF9900" />
+          <stop offset="90%" stopColor="#FFC68D" />
+        </linearGradient>
+        <pattern
+          id="wave"
+          x="0"
+          y="0"
+          width="120"
+          height="20"
+          patternUnits="userSpaceOnUse"
+        >
+          <path
+            id="wavePath"
+            d="M-40 9 Q-30 7 -20 9 T0 9 T20 9 T40 9 T60 9 T80 9 T100 9 T120 9 V20 H-40z"
+            mask="url(#mask)"
+            fill="url(#gradient)"
+          >
+            <animateTransform
+              attributeName="transform"
+              begin="0s"
+              dur="1.5s"
+              type="translate"
+              from="0,0"
+              to="40,0"
+              repeatCount="indefinite"
+            />
+          </path>
+        </pattern>
+      </defs>
+      <text
+        textAnchor="middle"
+        x="50"
+        y="15"
+        fontSize="17"
+        fill="url(#wave)"
+        fillOpacity="0.9"
+      >
+        LOADING
+      </text>
+      <text
+        textAnchor="middle"
+        x="50"
+        y="15"
+        fontSize="17"
+        fill="url(#gradient)"
+        fillOpacity="0.1"
+      >
+        LOADING
+      </text>
+    </SVG>
   </Container>
 );
 
