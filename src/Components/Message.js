1b4387e6306b4875f9dade543db612cd1f4695bf
rovin0805 <qhdrn67@naver.com>
2021-01-08 11:33:08 +0900

    error msg 컨테이너 삭제

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -3,14 +3,8 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import { BiError } from "react-icons/bi";
 
-const Container = styled.div`
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  margin-top: 50px;
-`;
-
 const Text = styled.span`
+  margin-top: 50px;
   color: ${(props) => props.color};
   font-size: 30px;
   font-weight: bold;
@@ -19,12 +13,10 @@ const Text = styled.span`
 `;
 
 const Message = ({ text, color }) => (
-  <Container>
-    <Text color={color}>
-      <BiError color="#D3D3D3" style={{ marginRight: 5 }} size={40} />
-      {text}
-    </Text>
-  </Container>
+  <Text color={color}>
+    <BiError color="#D3D3D3" style={{ marginRight: 5 }} size={40} />
+    {text}
+  </Text>
 );
 
 Message.propTypes = {
