eadbf7df7cd0f9055d78ef7e7428002339b210bc
rovin0805 <qhdrn67@naver.com>
2021-01-08 14:47:08 +0900

    Msg css 수정

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -4,12 +4,13 @@ import styled from "styled-components";
 import { BiError } from "react-icons/bi";
 
 const Text = styled.span`
-  margin-top: 50px;
+  margin-top: 100px;
   color: ${(props) => props.color};
   font-size: 30px;
   font-weight: bold;
   display: flex;
   align-items: center;
+  justify-content: center;
 `;
 
 const Message = ({ text, color }) => (
