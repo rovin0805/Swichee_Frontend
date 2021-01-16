e3b368a9f9a6c3beb341582fb747ddd0cb8f0751
kypark7 <minequinto2@gmail.com>
2021-01-16 21:11:29 +0900

    company

--- src/Routes/Company/CompanyPresenter.js
+++ src/Routes/Company/CompanyPresenter.js
@@ -1,30 +1,12 @@
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Loader from "Components/Loader";
-// import nob from "Assets/nob.png";
-// import nob2 from "Assets/nob2.png";
-// import logo from "Assets/swichee.png";
-// import poh from "Assets/poh.png";
-// import logo2 from "Assets/logo.png";
-
-const SideImg = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const BoxContent = styled.div`
-  font-size: 15px;
-  color: gray;
-  width: 100vw;
-  justify-content: center;
-  text-align: center;
-  flex-direction: column;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50px;
-  line-height: 150%;
-`;
+import "./CompanyPresenter.css";
+import main_submenu1 from "Assets/main_submenu1.png";
+import main_home from "Assets/main_home.png";
+import main_background from "Assets/main_background.png";
+import main_music from "Assets/main_music.png";
+import logo from "Assets/logo.png";
 
 const Container = styled.div`
   display: flex;
@@ -37,47 +19,14 @@ const Container = styled.div`
   padding: 0px 0px 0px 0px;
 `;
 
-const Content = styled.div`
-  width: 100vw;
-  justify-content: center;
-  text-align: center;
-  flex-direction: column;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50px;
-  line-height: 150%;
-`;
-
-const Letter1 = styled.div`
-  font-size: 15px;
-  color: gray;
-  margin: 10px 10px 10px 10px;
-`;
-const Letter2 = styled.div`
-  font-size: 15px;
-  color: black;
-  margin: 10px 10px 10px 10px;
-`;
-
 const MainTitle = styled.div`
-  width: 100%;
-  background: linear-gradient(
-    to bottom,
-    #ff9900,
-    #ff9900,
-    #ff9900,
-    #ff9900,
-    #ff9900,
-    #ff9900,
-    #ff9900,
-    #ff9900,
-    white
-  );
-  color: white;
-  padding: 100px 0px 10px 0px;
+  width: 800px;
+  padding: 50px 0px 0px 55px;
   font-weight: bold;
   font-size: 17px;
-  text-align: center;
+  
+  margin: 0 auto;
+  background-color:white;
   @media only screen and (min-width: 508px) {
   }
   @media only screen and (max-width: 699px) {
@@ -88,27 +37,29 @@ const MainTitle = styled.div`
   }
 `;
 
-const Main = styled.div`
-  width: 100%;
-  background: white;
-  color: black;
-  padding: 50px 0px 0px 0px;
-  font-weight: bold;
-  font-size: 17px;
-  text-align: center;
-  margin-bottom: 30px;
-  @media only screen and (max-width: 768px) {
-  }
-  @media only screen and (min-width: 768px) and (max-width: 1024px) {
-  }
-  @media only screen and (min-width: 1025px) {
-  }
+const Badge = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 15px;
+  margin-right: 0px;
 `;
 
-const Logo = styled.div`
-  border-radius: 30%;
-  overflow: hidden;
-  margin: 0px 0px 0px 5px;
+const Mainpic = styled.img`
+    margin-left:13%;
+`;
+
+const Smallfont = styled.div`
+    font-size: 20px;
+    color: black;
+    width: 100%;
+    text-align: center;
+`;
+
+const Midfont = styled.div`
+    font-size: 21px;
+    color: black;
+    width: 600px;
+    text-align: center;
 `;
 
 const CompanyPresenter = ({ loading, error }) => {
@@ -117,27 +68,24 @@ const CompanyPresenter = ({ loading, error }) => {
       {loading ? (
         <Loader />
       ) : (
-        <Container>
+        <Container >
           <div>
-            <MainTitle>
-              <Content>
-                인디펜던트 크리에이터들을 <br />
-                위해 제작된 <br />
-                SWICHEE <br />
-              </Content>
-              <Content>
-                {/* <img src={nob} alt="nob" width="300px" /> */}
-              </Content>
-            </MainTitle>
-
-            <Main>
-              <Content>
-                우리 모두
-                {/* <img src={logo} alt="logo" height="20px" /> */}
-                에서 안전하게 만나요!
-                <br />
-              </Content>
-              <BoxContent>
+          <MainTitle id="particle-container">
+        <div class="masked-copy texture-orange big-type" >
+          <h2>인디펜던트 크리에이터들을</h2><br/>
+          <h2>위해 제작된</h2><br/>
+          <p>SWICHEE</p><br/>
+          <Mainpic src={main_submenu1} alt="main_submenu1" width="650px" />
+        </div>
+        </MainTitle>
+        <br/>
+        <MainTitle>
+        <div class="masked-copy texture-orange big-type" >
+          <h2>우리 모두</h2><br/>
+          <p>SWICHEE</p>
+                <h2>에서 안전하게 만나요!</h2>
+                <br/><br/>
+                <Smallfont>
                 스위치에서는 이미 많은 크리에이터가
                 <br />
                 오프라인의 어려움을 이겨내고 온라인을 통해
@@ -145,87 +93,76 @@ const CompanyPresenter = ({ loading, error }) => {
                 좋은 콘텐츠를 제작하고 팬들에게 선보이며
                 <br />
                 건강한 창작 생태계를 만들어가고 있습니다.
-                <br />
-                <br />
+                <br /><br />
                 어디에서도 볼 수 없었던 크리에이터와
                 <br />
                 팬들과의 돈독한 관계를 만들어가세요.
-                <br />
-              </BoxContent>
-              <Content>
-                {/* <img src={poh} alt="poh" width="100px" />
-                <img src={poh} alt="poh" width="100px" />
-                <img src={poh} alt="poh" width="100px" />
-                <img src={poh} alt="poh" width="100px" /> */}
-              </Content>
-              <BoxContent>
-                끊김없는 스트리밍으로 마음껏 즐겨보세요.
-                <br />
-              </BoxContent>
-              <Content>
-                <SideImg>
-                  <Letter2>
-                    {/* <img src={logo2} alt="logo" height="15px" /> 내가 만들어가는 */}
-                    홈 화면
-                    <br />
-                    <Letter1>
-                      내가 좋아하는 크리에이터를 팔로우하고
+                <br /><br />
+                </Smallfont>
+                </div>
+        </MainTitle>
+        <br /><br />
+        <MainTitle>
+        <div class="masked-copy texture-orange big-type" >
+        <p>#</p><h2>내가 만들어가는</h2> <p>홈화면</p><br/><br/>
+          <Badge>
+          <Midfont>
+          <h2>내가 좋아하는 크리에이터를<br />
+             팔로우하고 새로운 콘텐츠를<br />
+              가장 먼저 만나보세요.
+                </h2><br/>
+          </Midfont>
+          <img src={main_home} alt="main_home" width="470px" />
+          </Badge>
+        </div>
+        </MainTitle>
+        <br /><br />
+        <MainTitle>
+        <div class="masked-copy texture-orange big-type" >
+        <p>#</p>
+          <h2>배경화면, 웹툰 </h2><p>그 무엇이든</p><br/><br/><br />
+          <Badge>
+          <img src={main_background} alt="main_background" width="450px" />
+          <Midfont>
+          게시글에 최대 60장까지 <br />
+          업로드할 수 있습니다.<br />
+          평소에 불편하게 공유하던<br />
+          콘텐츠를 가장 편리하게<br />
+          관리하세요.
+          <br/><br/>
+          </Midfont>
+          </Badge>
+        </div>
+        </MainTitle>
+        <br /><br />
+        <MainTitle>
+        <div class="masked-copy texture-orange big-type" >
+        <p>#</p>
+          <h2>동영상, 음악 </h2><p>스트리밍</p><br/><br/>
+          <Badge>
+          <Midfont>
+                      누구나 유통사를 거치지 않고<br />
+                      가장 빠르게 자작곡을 <br />
+                      팬들에게 선공개<br /> 할 수 있습니다.
                       <br />
-                      새로운 콘텐츠를 가장 빨리 만나보세요.
-                    </Letter1>
-                  </Letter2>
-                  {/* <img src={poh} alt="poh" width="100px" /> */}
-                  <br />
-                </SideImg>
-              </Content>
-              <Content>
-                <SideImg>
-                  <Letter2>
-                    {/* <img src={logo2} alt="logo" height="15px" /> 배경화면, 웹툰 */}
-                    그 무엇이든
-                    <br />
-                    <Letter1>
-                      게시글에 최대 60장까지 업로드할 수<br />
-                      있습니다. 평소에 불편하게 공유하던
                       <br />
-                      콘텐츠를 가장 편리하게 관리하세요.
-                    </Letter1>
-                  </Letter2>
-                </SideImg>
-                {/* <img src={nob2} alt="nob2" width="300px" /> */}
-              </Content>
-              <Content>
-                <SideImg>
-                  <Letter2>
-                    {/* <img src={logo2} alt="logo" height="15px" /> 음악, 동영상 */}
-                    스트리밍
-                    <br />
-                    <Letter1>
-                      누구나 유통사를 거치지 않고 가장 빠르게
-                      <br />
-                      자작곡을 팬들에게 선공개 할 수 있습니다.
-                      <br />
-                    </Letter1>
                     지원 파일 형식
-                    <Letter1>
+                    <br />
                       오디오 ACC,m4a,mp3
                       <br />
                       비디오 mp4, mov, 3gp
                       <br />
-                    </Letter1>
-                  </Letter2>
-                  {/* <img src={poh} alt="poh" width="100px" /> */}
-                  <br />
-                </SideImg>
-              </Content>
-            </Main>
-            <Content></Content>
-          </div>
-        </Container>
-      )}
-    </>
-  );
-};
+                      </Midfont>
+          <img src={main_music} alt="main_music" width="470px" />
+          </Badge>
+        </div>
+        </MainTitle>
+</div>
+          </Container>
+        )}
+      </>
+    );
+  };
 
 CompanyPresenter.propTypes = {
   loading: PropTypes.bool.isRequired,
